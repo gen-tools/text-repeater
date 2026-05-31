bac06a6d31b775cca1a5b1ead1bca12283081129
gen-tools <shahazaibseo038@gmail.com>
2026-05-31 18:56:50 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/', '/_next/'],
       },
     ],
-    sitemap: 'https://textrepeater.com/sitemap.xml',
+    sitemap: 'https://text-repeater0.vercel.app/sitemap.xml',
   }
 }
